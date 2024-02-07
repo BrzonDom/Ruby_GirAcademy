a02e1ad8ee6d7874cb78d2330d21155c1c0e2e1e
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-07 14:33:47 +0100

    4.0 - Data Types

---
+++ 04_Data Types.rb
@@ -0,0 +1,20 @@
+
+  # Strings are defined by enclosing a text within
+  # a single (”) or double (“”) quotes.
+string = "Hello world"
+
+  # Ruby can handle both Integers and floating point numbers.
+  # According to their size, there are two types of integers,
+  # one is Bignum and second is Fixnum.
+integer = 50
+float = 1.5
+
+  # Boolean data type represents only one bit
+  # of information either true or false.
+boolean = true
+boolean = false
+
+  # nil is a special value that denotes the absence of any value.
+  # Nil is an object of NilClass. nil is Ruby’s way of
+  # referring to nothing or void.
+nothing = nil
