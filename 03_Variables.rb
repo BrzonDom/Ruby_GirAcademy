29db8cca25fcc865c1e24f7bb3ad7ceb82606011
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-07 14:01:41 +0100

    3.0 - Variables

---
+++ 03_Variables.rb
@@ -0,0 +1,14 @@
+
+name = "John"
+age = "70"
+
+puts "There once was a man named John"
+puts "he was 70 years old."
+puts "He really liked the name John"
+puts "but didn't like being 70."
+puts
+
+puts ("There once was a man named " + name)
+puts ("he was " + age + " years old.")
+puts ("He really liked the name " + name)
+puts ("but didn't like being " + age + ".")
